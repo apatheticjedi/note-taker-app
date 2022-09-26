63aed6645b70a093cfba108527f5ecd390c4843c
Duder <thedudeabides1981@hotmail.com>
2022-09-25 21:15:34 -0700

    created post request

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,20 +3,31 @@ const router = require('express').Router();
 const fs = require('fs');
 const data = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json"), "utf8")) || [];
 const { notes } = require('../db/db.json');
+const { v4: uuidv4 } = require('uuid');
 
 
 router.get('/notes', (req, res) => {
     res.send(data);
+    console.log(data);
 });
 
-// router.post('/notes', (req, res) => {
-//     req.body.id = notes.length.toString();
-//     if (!title || !text) {
-//         res.status(400).send('The note is not properly formatted.');
-//     } else {
-//         const note = createNewNote(req.body, notes);
-//         res.json(note);
-//     }
-// });
+router.post('/notes', (req, res) => {
+    console.log(req.body);
+    const { title, text } = req.body;
+    if (title && text) {
+        const newNote = {
+            title,
+            text, 
+            id: uuidv4()
+        }
+        data.push(newNote)
+        console.log(data);
+        fs.writeFileSync(
+            path.join(__dirname, '../db/db.json'),
+            JSON.stringify(data), null, 2);
+    }
+    
+});
+
 
 module.exports = router;
